5f7103ddbdf92013be75e570fd0d4c4212c99c40
vilotiv <122198710+vilotiv@users.noreply.github.com>
2023-04-18 11:53:05 +0500

    Update readme.mk

--- R-Plat/network/readme.mk
+++ R-Plat/network/readme.mk
@@ -1,1 +1,4 @@
 for TP Rplat
+
+
+nmcli c m p3p1 ethernet.mtu 9000;nmcli c m p3p2 ethernet.mtu 9000;nmcli c m id bnd1 ethernet.mtu 9000;nmcli c m id bnd1 bond.options xmit_hash_policy=layer3+4;systemctl restart NetworkManager
